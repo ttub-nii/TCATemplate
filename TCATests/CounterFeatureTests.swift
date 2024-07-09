e2506bd45a87fe56a40fea3ea8afdc5a9f3894c2
Subin Hwang <ostarangelo@naver.com>
2024-07-09 15:53:16 +0900

    테스팅(5) network request 추가

--- TCATests/CounterFeatureTests.swift
+++ TCATests/CounterFeatureTests.swift
@@ -47,4 +47,18 @@ final class CounterFeatureTests: XCTestCase {
             $0.isTimerRunning = false
         }
     }
+    
+    func testNumberFact() async {
+        let store = TestStore(initialState: CounterFeature.State()) {
+            CounterFeature()
+        }
+        
+        await store.send(.factButtonTapped) {
+            $0.isLoading = true
+        }
+        await store.receive(\.factResponse, timeout: .seconds(1)) {
+            $0.isLoading = false
+            $0.fact = "???"
+        }
+    }
 }
