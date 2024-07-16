8f7b2efb7856875d716d7b4540cfcbb3bdbf79cf
Subin Hwang <ostarangelo@naver.com>
2024-07-16 16:52:40 +0900

    Reduce 합성 - 부모feature에서 동작 처리

--- TCA/App/AppFeature.swift
+++ TCA/App/AppFeature.swift
@@ -21,6 +21,9 @@ struct AppFeature {
     }
     
     var body: some ReducerOf<Self> {
+        /// scope가 없어도 compile 은 되고 runtime에서 동작 안하는 이유
+        /// ReducerOf가 내부에서 각각을 Reducer 로 만들고 merge.
+        /// runtime에서 keypath id 로 찾기 때문에 compile 은 되는듯.
         Scope(state: \.tab1, action: \.tab1) {
             CounterFeature()
         }
@@ -30,7 +33,23 @@ struct AppFeature {
         }
         
         Reduce { state, action in
-            // Core logic of the app feature
+            switch action {
+            case .tab1(.decrementButtonTapped):
+                state.tab1.fact = "testtest"
+                state.tab2.fact = "44444444"
+            case .tab1(.incrementButtonTapped):
+                break
+            case .tab1(.toggleTimerButtonTapped):
+                break
+            case .tab1(.timerTick):
+                break
+            case .tab1(.factButtonTapped):
+                break
+            case .tab1(.factResponse(_)):
+                break
+            case .tab2(_):
+                break
+            }
             return .none
         }
     }
